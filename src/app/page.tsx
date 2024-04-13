2bc6d7b790047b88d5c4e1b1aa130ad9be518f9c
Chaitanya Bajpai <cb7chaitanya@gmail.com>
2024-04-13 12:34:42 +0530

    reinitialized project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,8 @@
 import React from 'react';
+import { Button } from '@/components/hero/Button';
+import { Logo } from '@/components/hero/Logo';
+import Link from 'next/link';
+import AddModal from '@/components/AddModal';
 
 /*declare module 'react' {
   interface JSX.IntrinsicElements {
@@ -8,10 +12,12 @@ import React from 'react';
 
 export default function Home() {
   return (
-    <div className=" h-screen">
+    <div className="h-screen hero-section">
       <div className="flex flex-col items-center justify-center h-screen">
-        <p className="text-xl text-gray-400 mb-8 text-left font-semibold"><span className="text-white font-semibold">SnippSavvy is the ultimate code snippet management tool.</span> We at SnipSavvy aim to improve developer experience by providing you with a platform where you can store and share code snippets</p>
-        <button className="px-3 py-2 bg-white text-zinc-800 font-semibold rounded-3xl hover:bg-blue-400 duration-300 glow-gradient">Make your first workspace here</button>
+        <Logo className='text-6xl'/>
+        <p className="text-xl text-gray-400 mb-8 text-left font-semibold"><span className="text-white font-semibold">It is the ultimate code snippet management tool.</span> We at SnipSavvy aim to improve developer experience by providing you with a platform where you can store and share code snippets</p>
+        <Link href='/'><Button description='Create your first Workspace'></Button></Link>
+        <AddModal/>
       </div>
     </div>
   )
--- src/components/navbar.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Navbar() {
-  return (
-    <div className='border-b border-zinc-600 text-white font-bold text-3xl py-4 bg-black'>SnipSavvy</div>
-  )
-}
-
-export default Navbar
