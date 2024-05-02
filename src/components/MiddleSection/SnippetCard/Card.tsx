987eeb864a85de42504760db0b625c99dfabee7e
Sahil <sahilaroraji2002@gmail.com>
2024-05-02 21:29:42 +0530

    updating the snippet CARD

--- src/components/MiddleSection/SnippetCard/Card.tsx
+++ src/components/MiddleSection/SnippetCard/Card.tsx
@@ -1,38 +1,52 @@
 import React from "react";
-import DeleteIcon from '@mui/icons-material/Delete';
-import EditNoteIcon from '@mui/icons-material/EditNote';
-import MoreVertIcon from '@mui/icons-material/MoreVert';
+import DeleteIcon from "@mui/icons-material/Delete";
+import EditNoteIcon from "@mui/icons-material/EditNote";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import Image from "next/image";
+
 interface SnippetCardProps {
   title: string;
   code: string;
+  description: string;
+  tags: string[];
 }
 
-const SnippetCard: React.FC<SnippetCardProps> = ({ title, code }) => {
+const SnippetCard: React.FC<SnippetCardProps> = ({
+  title,
+  code,
+  description,
+  tags,
+}) => {
+  const languageIcon = "https://img.icons8.com/?size=50&id=22181&format=png";
+
   return (
     <div
-      style={{ width: "23vw", height: "14vh",borderRadius:"0.6rem" }}
-      className="mb-6 mr-4 rounded-lg flex justify-center items-center  border-zinc-800 rounded justify-between hover:bg-zinc-950 rounded-lg hover:border-zinc-400 hover:border-2 overflow-hidden"
+      className="flex items-start border-zinc-800 rounded-lg hover:bg-zinc-950 hover:border-zinc-100 hover:border-4 overflow-hidden mb-6 mr-4 p-2"
+      style={{
+        width: "23vw",
+        height: "14vh",
+        borderRadius: "0.6rem",
+      }}
     >
-      {/* <div className="w-2/12 flex flex-col items-center pt-3">LO</div> */}
-      <div className="w-8/12 flex flex-col h-full pt-2 ">
-        <div className="text-xl font-bold text-gray-300 flex justify-start pl-4">{title}</div>
-        <div className="text-md font-semibold mb-2 text-gray-500 h-20 ">
+      <div className="flex items-center mr-2">
+        <Image src={languageIcon} alt="Icon" width={40} height={40} />
+      </div>
+
+      <div className="flex flex-col justify-start">
+        <div className="text-xl font-semibold font-sans text-gray-300 mb-2 pl-2">
+          {title}
+        </div>
+        <div className="text-md font-semibold text-gray-500 h-20 overflow-hidden pl-2">
           {code.substring(0, 60)}
         </div>
       </div>
-      <div className="w-4/12 flex flex-col  justify-between items-center h-[90%]">
-        <div className="flex h-10">
-          {/* <div className="rounded full h-10 w-8  border-zinc-700 hover:border-zinc-400"><DeleteIcon/></div>
-          <div className=" rounded full h-10 w-8 border-zinc-700 hover:border-zinc-400"><EditNoteIcon/></div> */}
-          <div className="rounded full h-10 w-8  border-zinc-700 hover:border-zinc-400"><MoreVertIcon/></div>
-        </div>
-       
-        <div className="flex items-center justify-center">
-          {/* <div className="border-2 rounded-lg h-8 w-8 mb-2 mr-2"></div> */}
-          {/* <div>2min</div> */}
+      <div className="flex items-center justify-center ml-auto">
+        <div className="rounded-full h-10 w-10 flex items-center justify-center border-zinc-700 hover:border-zinc-400">
+          <MoreVertIcon />
         </div>
       </div>
     </div>
   );
 };
+
 export default SnippetCard;
--- src/components/MiddleSection/SnippetSection.tsx
+++ src/components/MiddleSection/SnippetSection.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React from "react";
-import { useState,useEffect } from "react";
+import { useState, useEffect } from "react";
 import SnippetCard from "./SnippetCard/Card";
 import Link from "next/link";
 // import Collection from "../workspace/Collection";
@@ -23,7 +23,7 @@ const SnippetSection: React.FC<SnippetSectionProps> = () => {
   const snippet = searchParams.get("snippet")
     ? searchParams.get("snippet")
     : "";
- 
+
   const updateUrl = (name: string) => {
     const workspace = searchParams.get("workspace");
     const collection = searchParams.get("collection");
@@ -43,64 +43,62 @@ const SnippetSection: React.FC<SnippetSectionProps> = () => {
 
     setIsOpenSnippet(true);
   };
- 
-  
-  const [isSnippet, setIsSnippet] = useState<any>([]); //[FIX ME ] 
-  
-  useEffect(()=> {
+
+  const [isSnippet, setIsSnippet] = useState<any>([]); //[FIX ME ]
+
+  useEffect(() => {
     const fetchSnippets = async () => {
-      axios.get('https://snipsavvy.onrender.com/v1/api/snippet?cat_id=' + `${collection}`)
-      .then(response => {
-        setIsSnippet(response.data.data)
-        console.log(response.data.data)
-      }).catch(error => {
-        console.log(error)
-      })
-    }
-    collection && fetchSnippets()
-}, [collection])
-interface Snip{
-  _id: string;
-  title: string;
-  description :string;
-  code :string;
-}
+      axios
+        .get(
+          "https://snipsavvy.onrender.com/v1/api/snippet?cat_id=" +
+            `${collection}`
+        )
+        .then((response) => {
+          setIsSnippet(response.data.data);
+          console.log(response.data.data);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    };
+    collection && fetchSnippets();
+  }, [collection]);
+  interface Snip {
+    _id: string;
+    title: string;
+    description: string;
+    code: string;
+    tags: string[];
+  }
+  const languageIcon = "/icons/js-icon.png";
   return (
-    
     <div>
       {collection && (
         <div style={{ minHeight: "100vh" }} className="h-screen-full w-full">
-          <div
-            className={`${snippet ? "w-1/3 " : "vw-75"} flex flex-col `}
-          >
+          <div className={`${snippet ? "w-1/3 " : "vw-75"} flex flex-col `}>
             <div className="text-xl pl-4 mb-4">
               Get Started with Code Snippets...
             </div>
             {/* <hr className="h-2 text-gray-800 w-full mb-2" /> */}
-            
-              <div className="w-full flex mt-2">
-                <div className="w-full flex flex-wrap   overflow-hidden ">
-                  <div
+
+            <div className="w-full flex mt-2">
+              <div className="w-full flex flex-wrap   overflow-hidden ">
+                <div
                   className="flex flex-wrap "
-                    // className={`${snippet} ? "items-center justify-center h-5/6  " : " justify-center  h-fullitems-center"} overflow-y-scroll flex flex-wrap   h-full   `}
-                  >
-                    {isSnippet.map((snip:Snip) => (
-                      <button
-                        onClick={() => updateUrl(snip._id)}
-                        key={snip._id}
-                      >
-                        <SnippetCard {...snip} />
-                      </button>
-                    ))}
-                  </div>
+                  // className={`${snippet} ? "items-center justify-center h-5/6  " : " justify-center  h-fullitems-center"} overflow-y-scroll flex flex-wrap   h-full   `}
+                >
+                  {isSnippet.map((snip: Snip) => (
+                    <button onClick={() => updateUrl(snip._id)} key={snip._id}>
+                      <SnippetCard {...snip} />
+                    </button>
+                  ))}
                 </div>
               </div>
-            
+            </div>
           </div>
         </div>
       )}
     </div>
-    
   );
 };
 export default SnippetSection;
