cf94f2a539da9c3b304927013b2b224a28533d21
Sahil <sahilrohera10@gmail.com>
2024-05-12 15:38:49 +0530

    [BUGFIX] removed useSearchParams from page.tsx

--- src/app/workspace/SearchParamHandler.tsx
+++ src/app/workspace/SearchParamHandler.tsx
@@ -40,57 +40,52 @@ const SearchParamsHandler = () => {
   };
 
   return (
-    <Suspense>
-      <div>
-        <input
-          value={inpText}
-          onChange={(e) => setInpText(e.target.value)}
-          type="text"
-          className="w-full h-10 vh-5 bg-gray-800 text-white px-4 border-2 border-gray-400 hover:shadow-outline focus:outline-none border-l-0 rounded-r mb-4"
-          placeholder="Enter text..."
-        />
-        {inpText && (
-          <div className="w-[95%] m-auto mt-4 max-h-[40vh] overflow-auto">
-            {searchData &&
-              searchData.map((snippet: any, index: any) => (
-                <div
-                  key={index}
-                  onClick={() => updateURL(snippet)}
-                  className="shadow-2xl mr-2 flex flex-col gap-2 rounded-xl mb-6 cursor-pointer border-2 border-zinc-950 hover:border-2 hover:border-slate-500 p-2"
-                >
-                  <p className="flex">
-                    {" "}
-                    <IoIosArrowForward className="mt-1 mr-1" />
-                    workspace <IoIosArrowForward className="mt-1 mr-1" />
-                    collection <IoIosArrowForward className="mt-1 mr-1" />
-                    {snippet.title}{" "}
-                  </p>
-                  <div className="flex">
-                    <div>
-                      <p className="text-2xl font-semibold">
-                        {" "}
-                        {snippet.title}{" "}
-                      </p>
-                      <p> {snippet.description} </p>
-                    </div>
-                  </div>
-                  <div className="flex">
-                    {snippet.tags.map((tag: any, index: any) => (
-                      <p
-                        key={index}
-                        className="bg-black mr-2 p-1 pl-2 pr-2 rounded-xl"
-                      >
-                        {" "}
-                        {tag}{" "}
-                      </p>
-                    ))}
+    <div>
+      <input
+        value={inpText}
+        onChange={(e) => setInpText(e.target.value)}
+        type="text"
+        className="w-full h-10 vh-5 bg-gray-800 text-white px-4 border-2 border-gray-400 hover:shadow-outline focus:outline-none border-l-0 rounded-r mb-4"
+        placeholder="Enter text..."
+      />
+      {inpText && (
+        <div className="w-[95%] m-auto mt-4 max-h-[40vh] overflow-auto">
+          {searchData &&
+            searchData.map((snippet: any, index: any) => (
+              <div
+                key={index}
+                onClick={() => updateURL(snippet)}
+                className="shadow-2xl mr-2 flex flex-col gap-2 rounded-xl mb-6 cursor-pointer border-2 border-zinc-950 hover:border-2 hover:border-slate-500 p-2"
+              >
+                <p className="flex">
+                  {" "}
+                  <IoIosArrowForward className="mt-1 mr-1" />
+                  workspace <IoIosArrowForward className="mt-1 mr-1" />
+                  collection <IoIosArrowForward className="mt-1 mr-1" />
+                  {snippet.title}{" "}
+                </p>
+                <div className="flex">
+                  <div>
+                    <p className="text-2xl font-semibold"> {snippet.title} </p>
+                    <p> {snippet.description} </p>
                   </div>
                 </div>
-              ))}
-          </div>
-        )}
-      </div>
-    </Suspense>
+                <div className="flex">
+                  {snippet.tags.map((tag: any, index: any) => (
+                    <p
+                      key={index}
+                      className="bg-black mr-2 p-1 pl-2 pr-2 rounded-xl"
+                    >
+                      {" "}
+                      {tag}{" "}
+                    </p>
+                  ))}
+                </div>
+              </div>
+            ))}
+        </div>
+      )}
+    </div>
   );
 };
 
--- src/app/workspace/page.tsx
+++ src/app/workspace/page.tsx
@@ -51,104 +51,105 @@ const WorkspacePage: React.FC = () => {
   }, []);
 
   return (
-    <Suspense fallback={<div>Loading...</div>}>
+    <div>
       <Suspense fallback={<div>Loading...</div>}>
         <SearchParamsHandler />
+      </Suspense>
+
+      <div
+        style={{ width: "75vw" }}
+        className="fixed top-0 right-0 h-screen text-white bg-zinc-900"
+      >
         <div
           style={{ width: "75vw" }}
-          className="fixed top-0 right-0 h-screen text-white bg-zinc-900"
+          className="fixed top-0   pr-3 flex flex-col justify-between items-start bg-zinc-900  py-4"
         >
-          <div
-            style={{ width: "75vw" }}
-            className="fixed top-0   pr-3 flex flex-col justify-between items-start bg-zinc-900  py-4"
-          >
-            <div className="flex w-full justify-end">
-              <div className="flex w-full border-b-1 border-gray-700 shadow-md">
-                <div className=" ml-6 px-2 h-10 flex items-center rounded-l">
-                  <SearchIcon />
-                </div>
-                {open && (
-                  <div>
-                    <Modal
-                      className="backdrop-blur-sm"
-                      open={open}
-                      onClose={handleClose}
-                      aria-labelledby="modal-modal-title"
-                      aria-describedby="modal-modal-description"
-                    >
-                      <Box
-                        sx={{
-                          ...style,
-                          height: 500,
-                        }}
-                        className="bg-zinc-950  rounded-xl"
-                      >
-                        <Typography
-                          id="modal-modal-title"
-                          variant="h6"
-                          component="h2"
-                        >
-                          Search Snippet
-                        </Typography>
-                        <Typography
-                          id="modal-modal-description"
-                          sx={{ mt: 2, display: "flex" }}
-                        >
-                          <div className="border-2 ml-6 px-2 border-gray-400 h-10 flex items-center rounded-l">
-                            <SearchIcon />
-                          </div>
-                          <SearchParamsHandler />
-                        </Typography>
-                      </Box>
-                    </Modal>
-                  </div>
-                )}
-                <input
-                  type="text"
-                  className="w-full h-10 vh-5 bg-zinc-900 px-4 hover:shadow-outline focus:outline-none rounded-r text-lg text-gray-600 placeholder-gray-500 font-mono"
-                  placeholder="Find by Tag, Description , title..."
-                  onClick={handleOpen}
-                />
-                <div
-                  onClick={handleOpen}
-                  className="px-4 py-2 rounded-xl mb-2 flex   items-center text-xs text-gray-600 border-2 border-gray-600 shadow-lg   w-fit"
-                >
-                  <span>CTRL+K</span>
-                </div>
-                <div className="w-2/12 vh-6 flex justify-between pl-6 items-center">
-                  <button
-                    className="bg-blue-600 hover:bg-blue-400 duration-300 rounded-xl text-xl px-3 py-2 mb-2 text-white"
-                    onClick={handleAdd}
+          <div className="flex w-full justify-end">
+            <div className="flex w-full border-b-1 border-gray-700 shadow-md">
+              <div className=" ml-6 px-2 h-10 flex items-center rounded-l">
+                <SearchIcon />
+              </div>
+              {open && (
+                <div>
+                  <Modal
+                    className="backdrop-blur-sm"
+                    open={open}
+                    onClose={handleClose}
+                    aria-labelledby="modal-modal-title"
+                    aria-describedby="modal-modal-description"
                   >
-                    <IoIosAdd />
-                  </button>
-                  <Drawer
-                    className="fixed top-16 right-0"
-                    isOpen={openDrawer}
-                    setIsOpen={setOpenDrawer}
-                    isEditable={true}
-                    setIsEditable={setIsEditable}
-                    shared="false"
-                  />
+                    <Box
+                      sx={{
+                        ...style,
+                        height: 500,
+                      }}
+                      className="bg-zinc-950  rounded-xl"
+                    >
+                      <Typography
+                        id="modal-modal-title"
+                        variant="h6"
+                        component="h2"
+                      >
+                        Search Snippet
+                      </Typography>
+                      <Typography
+                        id="modal-modal-description"
+                        sx={{ mt: 2, display: "flex" }}
+                      >
+                        <div className="border-2 ml-6 px-2 border-gray-400 h-10 flex items-center rounded-l">
+                          <SearchIcon />
+                        </div>
+                        <SearchParamsHandler />
+                      </Typography>
+                    </Box>
+                  </Modal>
                 </div>
+              )}
+              <input
+                type="text"
+                className="w-full h-10 vh-5 bg-zinc-900 px-4 hover:shadow-outline focus:outline-none rounded-r text-lg text-gray-600 placeholder-gray-500 font-mono"
+                placeholder="Find by Tag, Description , title..."
+                onClick={handleOpen}
+              />
+              <div
+                onClick={handleOpen}
+                className="px-4 py-2 rounded-xl mb-2 flex   items-center text-xs text-gray-600 border-2 border-gray-600 shadow-lg   w-fit"
+              >
+                <span>CTRL+K</span>
+              </div>
+              <div className="w-2/12 vh-6 flex justify-between pl-6 items-center">
+                <button
+                  className="bg-blue-600 hover:bg-blue-400 duration-300 rounded-xl text-xl px-3 py-2 mb-2 text-white"
+                  onClick={handleAdd}
+                >
+                  <IoIosAdd />
+                </button>
+                <Drawer
+                  className="fixed top-16 right-0"
+                  isOpen={openDrawer}
+                  setIsOpen={setOpenDrawer}
+                  isEditable={true}
+                  setIsEditable={setIsEditable}
+                  shared="false"
+                />
               </div>
             </div>
-            <div className="mt-4 overflow-hidden p-4">
-              <SnippetSection />
+          </div>
+          <div className="mt-4 overflow-hidden p-4">
+            <SnippetSection />
 
-              <Drawer
-                className="fixed top-16 right-0"
-                isOpen={openDrawer}
-                setIsOpen={setOpenDrawer}
-                isEditable={isEditable}
-                setIsEditable={setIsEditable}
-                shared="false"
-              />
-            </div>
+            <Drawer
+              className="fixed top-16 right-0"
+              isOpen={openDrawer}
+              setIsOpen={setOpenDrawer}
+              isEditable={isEditable}
+              setIsEditable={setIsEditable}
+              shared="false"
+            />
           </div>
         </div>
-      </Suspense>
-    </Suspense>
+      </div>
+    </div>
   );
 };
 
